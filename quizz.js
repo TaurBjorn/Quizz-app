57a6702caebef8b3006ecf0c1e913cb7cf0c50ad
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-18 22:00:26 +0200

    Created an Array with objects, with custom questions

--- quizz.js
+++ quizz.js
@@ -1,5 +1,42 @@
 const quizData = [
     {
-        question: ''
+        question: 'In which period started mankind with agriculture and became sendentary?',
+        a: 'The Roman period',
+        b: 'The Paleolithic period',
+        c: 'The Neolithic period',
+        d: 'The Industrial Age',
+        correct: 'c'
+    },
+    {
+        question: "Which Roman emperor burned ancient Rome down?",
+        a: 'Caligula',
+        b: 'Augustus',
+        c: 'Claudius',
+        d: 'Nero',
+        correct: 'd'
+    },
+    {
+        question: 'When did Belgium became an independent country?',
+        a: '1492',
+        b: '1830',
+        c: '1969',
+        d: '476',
+        correct: 'b'
+    },
+    {
+        question: 'How did people in the 19th century communicated with each other when crossing distances?',
+        a: 'Facebook',
+        b: 'Email',
+        c: 'Letter',
+        d: 'Satellite connection',
+        correct: 'c'
+    },
+    {
+        question: 'What does an archaeologist study?',
+        a: 'Dinosaurs',
+        b: 'Apes',
+        c: 'Mankind',
+        d: 'Alien astronauts',
+        correct: 'c'
     }
 ]
