096c0be73cd4f30af141ff0e3eba8cf170702ed0
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-19 22:18:49 +0200

    DOM-manipulation and adding an event listener

--- quizz.js
+++ quizz.js
@@ -8,7 +8,7 @@ const quizData = [
         correct: 'c'
     },
     {
-        question: "Which Roman emperor burned ancient Rome down?",
+        question: 'Which Roman emperor burned ancient Rome down?',
         a: 'Caligula',
         b: 'Augustus',
         c: 'Claudius',
@@ -39,4 +39,46 @@ const quizData = [
         d: 'Alien astronauts',
         correct: 'c'
     }
-]
+];
+
+
+const question = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+const submitButton = document.getElementById('submit');
+
+
+
+let currentQuiz = 0;
+
+
+
+const loadQuiz = () => {
+   
+    const currentQuizData = quizData[currentQuiz];
+
+    question.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+    
+}
+
+loadQuiz();
+
+
+submitButton.addEventListener('click', () => {
+    currentQuiz++;
+    
+    if(currentQuiz < quizData.length) {
+        loadQuiz();       
+            
+    } else {
+        alert('You finished the quizz, congratulations!');
+    }
+});
